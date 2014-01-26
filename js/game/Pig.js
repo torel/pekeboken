b3758dc2feaee87961acd895dd2be34f50435995
Øyvind Heiene <oyvindheiene@hotmail.com>
2014-01-26 12:43:51 +0100

    fikset litt på gris.

--- js/game/Pig.js
+++ js/game/Pig.js
@@ -2,8 +2,9 @@ function Pig(context, xPos, yPos, scale) {
     this.gameObject = new Kiwi.GameObjects.Sprite(context, context.textures['pigAtlas'], xPos, yPos);
     this.gameObject.animation.createFromSequence(context.textures['pigAtlas'].sequences[3]);
     this.pigSnort = new Kiwi.Sound.Audio(kiwiGame, 'pigSnort', 0.5, false);
-
+    this.scale = scale;
     this.gameObject.transform.scale = scale;
+    this.left = true;
 
     this.animations = [
         { name: "walk" },
@@ -16,9 +17,15 @@ function Pig(context, xPos, yPos, scale) {
         { name: "limp" },
         { name: "limp_run" }*/
     ];
-    this.animation = this.animations[2];
-
+    this.animation = this.animations[0];
     this.gameObject.animation.play(this.animation.name);
+
+    this.characterScale = 1;
+    if (scale) {
+        this.characterScale = scale;
+        this.gameObject.scaleX = this.characterScale;
+        this.gameObject.scaleY = this.characterScale;
+    }
 
     this.onClickPig();
 }
@@ -33,9 +40,30 @@ Pig.prototype = {
 
     update: function() {
 
-    	if (this.animation.name == "walk") {
-    		this.gameObject.transform.x -= 1; 
+    	if (this.animation.name == "walk" || this.animation.name == "run") {
+
+    		if(this.left) {
+    			this.gameObject.transform.x -= ((this.animation.name == "run") ? 3 : 1);
+    			this.gameObject.scaleX = this.characterScale
+    		
+    		}
+
+    		else {
+    			this.gameObject.transform.x += ((this.animation.name == "run") ? 3 : 1);
+    			this.gameObject.scaleX = -this.characterScale
+    		}
+    		
+
+    		if(this.gameObject.transform.x > 700 && ! this.left) {
+                this.left = true;
+        	}
+        	else if(this.gameObject.transform.x < 50 && this.left) {
+                this.left = false;
+        	}
+
     	}
+
+    	
 
     },
 
--- js/game/Tractor.js
+++ js/game/Tractor.js
@@ -3,9 +3,15 @@ function Tractor(context, xPos, yPos, scale) {
     this.gameObject.animation.createFromSequence(context.textures['tractorAtlas'].sequences[0]);
     this.scale = scale
     this.gameObject.transform.scale = scale;
+    this.frameCount = 0;
 
-    this.animationName = "drive";
-    this.gameObject.animation.play(this.animationName);
+    this.animations = [
+        { name: "drive" },
+        { name: "driveSmoke" }
+    ];
+    this.animation = this.animations[0];
+    this.gameObject.animation.play(this.animation.name);
+    this.onClickTractor();
 
 }
 
@@ -18,10 +24,37 @@ Tractor.prototype = {
     },
 
     update: function() {
+    	console.log(this.animation.name)
+    	if (this.animation.name == "driveSmoke") {
+    		this.frameCount++;
+    	}
     	this.scale *= 0.9997
     	this.gameObject.transform.x -= 1;
     	this.gameObject.transform.y -= 0.17;
-    	this.gameObject.transform.scale = this.scale;  
+    	this.gameObject.transform.scale = this.scale;
+    	console.log(this.frameCount)
+    	
+    	if(this.frameCount >=70 ) {
+    		this.changeAnimation("drive")
+    		console.log("this.animation.name jfdklsfjdskhfjdksø", this.animation.name)
+    		
+    	}  
 
+    },
+
+    onClickTractor: function() {
+        this.frameCount = 0;
+        this.gameObject.input.onRelease.add(this.smoke, this);
+
+    },
+
+    smoke: function() {
+    	        console.log("klikka")
+    	this.changeAnimation("driveSmoke");
+    },
+
+    changeAnimation: function(animationName) {
+    	this.gameObject.animation.play(animationName)
+    	console.log(animationName)
     }
 }
